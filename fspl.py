75f29e5c8d119893262f1d0e47019bc338586a63
Michael <hirsch617@gmail.com>
2015-07-05 13:19:47 -0400

    array

--- fspl.py
+++ fspl.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from __future__ import division
-from numpy import log10, pi
+from numpy import log10, pi,atleast_1d
 
 class Link:
     def __init__(self,range_m,freq_hz,tx_dbm,rx_dbm):
-        self.range = range_m
+        self.range = atleast_1d(range_m)
         self.freq = freq_hz
         self.txpwr = tx_dbm
         self.rxsens = rx_dbm
@@ -22,23 +22,22 @@ class Link:
     def linkbudget(self):
         return self.txpwr - self.fspl() - self.rxsens
     def linkreport(self):
-        print('link margin {:0.1f}'.format(self.linkbudget()) + ' dB ')
+        print('link margin ' + str(self.linkbudget()) + ' dB ')
         print('based on isotropic 0dBi gain antennas and:')
-        print('free space path loss {:0.1f}'.format(self.fspl()) + ' dB ')
-        print('RX sensitivity {:0.1f}'.format(self.rxsens) + ' dBm')
-        print('TX power {:0.3f}'.format(self.power_watts()) + ' watts')
-        print('for Range [m]={:0.0f}'.format(self.range) +
-              ' Frequency [MHz]={:0.1f}'.format(self.freq_mhz()))
+        print('free space path loss ' + str(self.fspl()) + ' dB .')
+        print('RX sensitivity {:0.1f} dBm'.format(self.rxsens))
+        print('TX power {} watts'.format(self.power_watts()) )
+        print('for Range [m]= '+str(self.range) + '  Frequency [MHz]={:0.1f}'.format(self.freq_mhz()))
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
     p = ArgumentParser(description='trivial computation of free space loss -- no obstructions or fresnel zones are considered!')
-    p.add_argument('range_m', type=float, help='range between tx/rx [meters]')
-    p.add_argument('freq_hz', type=float, help='frequency [Hz]')
-    p.add_argument('tx_dbm',type = float, help='TX power [dBm]')
-    p.add_argument('rx_dbm',type = float, help='RX sensitivity [dBm]')
+    p.add_argument('-d','--range_m', type=float, help='range between tx/rx [meters]',nargs='+',required=True)
+    p.add_argument('-f','--freq_hz', type=float, help='frequency [Hz]',default=2450e6)
+    p.add_argument('-t','--tx_dbm',type = float, help='TX power [dBm]',default=-10.)
+    p.add_argument('-r','--rx_dbm',type = float, help='RX sensitivity [dBm]',default=-90.)
     ar = p.parse_args()
 
     Link(ar.range_m,ar.freq_hz,ar.tx_dbm,ar.rx_dbm).linkreport()
