a0f4813d0814dc32e9b1679d8f9da1c097428c7d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-03-15 23:57:42 -0400

    x

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ req = ['numpy','matplotlib','scipy','nose','seaborn']
 setup(name='radioutils',
       packages=['radioutils'],
       author='Michael Hirsch Ph.D.',
-      url='https://github.com/scienceopen/radioutils',
+      url='https://github.com/scivision/radioutils',
 	  )
